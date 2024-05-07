f3843a6b1e0fb5013b4f36ef1db59eeb0a4f1e82
ei-sugimoto <eikun1014@gmail.com>
2024-05-07 07:50:55 +0000

    pytesetで追記

--- test_cuda_validate.py
+++ test_cuda_validate.py
@@ -1,3 +1,15 @@
 import torch
 
-print(torch.cuda.is_available())
+
+def test_cuda_validate():
+    assert torch.cuda.is_available() is True
+
+
+def test_cuda_validate_fail():
+    assert torch.cuda.is_available() is not None
+
+
+def test_cuda_get_device_name():
+    device_name = torch.cuda.get_device_name()
+    assert device_name == 'NVIDIA GeForce RTX 4060'
+    assert device_name != 'not found'
