7f5dc357b54a6714fb6b19f4f64682fe03835b5c
ei-sugimoto <eikun1014@gmail.com>
2024-05-07 14:26:55 +0000

    add test directory and mypy.ini

---
+++ test/test_cuda_kernel.py
@@ -0,0 +1,23 @@
+import math
+
+import numpy as np
+from numba import cuda
+
+a = np.arange(8192, dtype=np.float32)
+a = np.full(8192, 3, dtype=np.float32)
+out = np.empty_like(a)
+
+
+@cuda.jit
+def gpu_sqrt_kernel(x: np.ndarray, out: np.ndarray):
+    idx = cuda.grid(1)  # type: ignore
+    if idx < x.size:  # Avoid out-of-bounds access
+        out[idx] = math.sqrt(x[idx])
+
+
+threads_per_block = 256
+blocks_per_grid = (a.size + (threads_per_block - 1)) // threads_per_block
+gpu_sqrt_kernel[blocks_per_grid, threads_per_block](a, out)  # type: ignore
+
+print(a)
+print(out)
